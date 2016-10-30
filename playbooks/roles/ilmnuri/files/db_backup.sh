6eed3d03cecc92de4a7c6c333444bf11cc0e30b5
Bekzot Azimov <bekzot.azimov@yahoo.com>
2016-10-30 15:00:04 +0000

    added stats and db

--- playbooks/roles/ilmnuri/files/db_backup.sh
+++ playbooks/roles/ilmnuri/files/db_backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-/usr/bin/sqlite3 /usr/share/nginx/html/app/tokens.db .dump > /root/backups/token_db_$(date +"%m_%d_%Y").bak
+dt=$(date +"%m_%d_%Y")
+/usr/bin/sqlite3 /usr/share/nginx/html/app/tokens.db .dump > /root/backups/token_db_$dt.bak
+/usr/bin/sqlite3 /usr/share/nginx/html/stats.db .dump > /root/backups/stats_db_$dt.bak
 
 cd /root/backups/
 /usr/bin/aws s3 sync . s3://dbbackup.ilmnuri.com/
---
+++ playbooks/roles/ilmnuri/files/stats.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+from datetime import datetime
+import os
+import gzip
+
+con = lite.connect('/usr/share/nginx/html/stats.db')
+
+dt = datetime.now().strftime('%Y%m%d')
+dt_value = datetime.now().strftime('%b-%d-%Y')
+log_file = '/var/log/nginx/access.log-{0}.gz'.format(dt)
+count = 0
+
+if os.path.isfile(log_file):
+    with gzip.open(log_file, 'r') as f:
+        for line in f:
+            if "mp3" in line:
+                count += 1
+    with con:
+        cur = con.cursor()
+        if not cur.execute('select 1 from stats where Name = ?;', (dt_value,)):
+            cur.execute("INSERT INTO stats VALUES(?,?);", (dt_value, count))
+
