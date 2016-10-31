f40f1130d9274f7c9e4c7db9c34ae14279891905
Bekzot Azimov <bazimov@gogoair.com>
2016-10-30 19:09:04 -0500

    backup databases

--- playbooks/roles/ilmnuri/files/db_backup.sh
+++ playbooks/roles/ilmnuri/files/db_backup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dt=$(date +"%m_%d_%Y")
-/usr/bin/sqlite3 /usr/share/nginx/html/app/tokens.db .dump > /root/backups/token_db_$dt.bak
-/usr/bin/sqlite3 /usr/share/nginx/html/stats.db .dump > /root/backups/stats_db_$dt.bak
+/usr/bin/sqlite3 /usr/share/nginx/html/app/tokens.db .dump > /root/backups/token_db_"$dt".bak
+/usr/bin/sqlite3 /usr/share/nginx/html/stats.db .dump > /root/backups/stats_db_"$dt".bak
+/usr/bin/sqlite3 /usr/share/nginx/html/flags.db .dump > /root/backups/flags_db_"$dt".bak
 
 cd /root/backups/
 /usr/bin/aws s3 sync . s3://dbbackup.ilmnuri.com/
